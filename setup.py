b3caf579d6745b58e20864502ba0f207fa7666e5
Rob Kent <rob@Robs-MBP.dyn.lifesci.dundee.ac.uk>
2018-03-09 12:16:46 +0000

    Updates to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,20 @@ from setuptools import setup
 setup(
     name="PyQt5Singleton",
     version="0.1",
-    author="Rob Kent"
+    author="Rob Kent",
+    author_email="jazzycamel@googlemail.com",
+    description="A simple singleton implementation to work with PyQt5",
+    license="MIT",
+    url="https://github.com/jazzycamel/PyQt5Singleton",
+    packages=['PyQt5Singleton'],
+    install_requires=["PyQt5","sip"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+    ]
 )
