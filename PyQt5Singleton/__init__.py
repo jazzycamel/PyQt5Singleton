5cdf57e78e8ea0d4225fa9529f7c0ddbd9d095ea
Rob Kent <rob@Robs-MBP.dyn.lifesci.dundee.ac.uk>
2018-03-09 12:20:32 +0000

    Removing usage example from source code

--- PyQt5Singleton/__init__.py
+++ PyQt5Singleton/__init__.py
@@ -11,23 +11,3 @@ class Singleton(pyqtWrapperType, type):
         if cls.instance is None:
             cls.instance=super().__call__(*args, **kw)
         return cls.instance
-
-if __name__=="__main__":
-    from PyQt5.QtCore import QObject, pyqtSlot, pyqtProperty
-
-    class MyObject(QObject, metaclass=Singleton):
-        def __init__(self, parent=None, **kwargs):
-            super().__init__(parent, **kwargs)
-
-            self._x=0
-
-        def x(self): return self._x
-
-        @pyqtSlot(int)
-        def setX(self, x): self._x=x
-
-        x=pyqtProperty(int, x, setX)
-
-    print(MyObject().x)
-    MyObject().x=1
-    print(MyObject().x)
